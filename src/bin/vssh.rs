af68bf163b541737a156d4ff5bf8b96c2f23e6f8
Eliana Oden <eliana.oden14@gmail.com>
2025-02-25 20:51:45 -0600

    Tried to complete level 1 and add level 2 parts

--- src/bin/vssh.rs
+++ src/bin/vssh.rs
@@ -3,7 +3,9 @@ use std::ffi::CString;
 use std::io::{stdin, Write};
 use std::path::Path;
 use std::process::{exit, ExitCode};
-use nix::unistd::{chdir, execvp, fork, getpid, ForkResult};
+use nix::fcntl::{open, OFlag};
+use nix::sys::stat::Mode;
+use nix::unistd::{chdir, dup2, execvp, fork, getpid, ForkResult};
 use nix::sys::wait::waitpid;
 
 fn main() {
@@ -26,29 +28,70 @@ fn main() {
             }
             continue;
         }
+
+        let mut parts: Vec<&str> = input.split_whitespace().collect();
+        let mut background = false;
+        let mut input_file = None;
+        let mut output_file = None;
+
+        if parts.last() == Some(&"&"){
+            background = true;
+            parts.pop();
+        }
+
+        let mut i = 0;
+        while i< parts.len(){
+            if parts[i] == "<" && i + 1 < parts.len(){
+                input_file = Some(parts.remove(i+1));
+                parts.remove(i);
+            }else if parts[i] == ">" && i+1 < parts.len(){
+                output_file = Some(parts.remove(i+1));
+                parts.remove(i);
+            }else{
+                i +=1;
+            }
+        }
+
+
         
-        let mut argv = Vec::new();
-        for arg in [input, "src/bin/vssh.rs"] {
-        argv.push(CString::new(arg).unwrap());
-        }
-        if argv.is_empty() {
-            continue;
-        }
+       
 
         match unsafe { fork() } {
             Err(e) => eprintln!("Fork failed: {}", e),
             Ok(fork_value) => match fork_value {
                 ForkResult::Parent { child } => {
-                    match waitpid(child, None) {
-                        Ok(status) => {
-                            println!("parent of {child} ({}) status: {status:?}", getpid());
-                        }
-                        Err(e) => {
-                            println!("I can't wait! {e}");
-                        }
+                    if !background{
+                        let _ = waitpid(child, None);
+                    }else{
+                        println!("Background process started: {}", child);
+
                     }
+                    
                 }
                 ForkResult::Child => {
+                    let mut argv = Vec::new();
+                    for arg in [input, "src/bin/vssh.rs"] {
+                    argv.push(CString::new(arg).unwrap());
+                    }
+                    if argv.is_empty() {
+                        continue;
+                    }
+                    if let Some(file) = input_file {
+                        if let Ok(fd) = open(file, nix::fcntl::OFlag::O_RDONLY, Mode::empty()) {
+                            let _ = dup2(fd, 0);
+                        } else {
+                            eprintln!("Failed to open input file");
+                            process::exit(1);
+                        }
+                    }
+                    if let Some(file) = output_file {
+                        if let Ok(fd) = open(file, OFlag::O_WRONLY | OFlag::O_CREAT | OFlag::O_TRUNC, Mode::S_IRUSR | Mode::S_IWUSR) {
+                            let _ = dup2(fd, 1);
+                        } else {
+                            eprintln!("Failed to open output file");
+                            process::exit(1);
+                        }
+                    }
                     if let Err(e) = execvp(&argv[0], &argv) {
                         println!("Error executing {}: {}", input, e);
                         process::exit(1);
