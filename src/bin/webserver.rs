c6fe0e7e1079af211dd02f786ba4508eb3328478
Eliana Oden <eliana.oden14@gmail.com>
2025-03-05 11:54:44 -0600

    Step 2

--- src/bin/webserver.rs
+++ src/bin/webserver.rs
@@ -3,9 +3,28 @@ use std::result::Result::Ok;
 use std::io::{Read, Write};
 use std::thread;
 
-fn handle_client(stream: TcpStream) {
+fn handle_client(mut stream: TcpStream) {
     let peer_addr = stream.peer_addr().expect("Could not get peer address");
-    println!("Received connection from: {}", peer_addr);
+    let mut buffer = [0; 500];
+    let mut request = String::new();
+
+    loop {
+        match stream.read(&mut buffer) {
+            Ok(n) => {
+                let msg = match std::str::from_utf8(&buffer[..n]) {
+                    Ok(s) => s,
+                    Err(_) => "",
+                };
+                request.push_str(msg);
+                if request.contains("\r\n\r\n") || request.contains("\n\n") {
+                    break;
+                }
+            }
+            Err(_e) => break,
+        }
+    }
+    
+    println!("Received request from {}:\n{}", peer_addr, request);
 }
 
 fn main() {
